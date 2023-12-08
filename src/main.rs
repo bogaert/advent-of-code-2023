be9c1a20c6079bdd966b5047d7ae8c3c541488bd
Christophe Bogaert <christophe@bogaert.se>
2023-12-08 12:43:05 +0100

    Load d1 input file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn print_first_5_lines_of_file() -> io::Result<()> {
+    // Path to the file in the parent directory
+    let path = Path::new("input/d1/input");
+
+    // Open the file
+    let file = File::open(&path)?;
+
+    // Use a buffered reader to read the file line-by-line
+    let reader = io::BufReader::new(file);
+
+    // Iterate over the lines and print the first 5
+    for (index, line) in reader.lines().enumerate() {
+        let line = line?;
+        println!("{}", line);
+
+        // Stop after 5 lines
+        if index >= 4 {
+            break;
+        }
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Call the function in main
+    if let Err(e) = print_first_5_lines_of_file() {
+        eprintln!("Error reading file: {}", e);
+    }
 }
