dc5e57c2ccda7d8f848fe111f5f4d1a57ef6075f
Christophe Bogaert <christophe@bogaert.se>
2023-12-09 11:07:06 +0100

    Add solution to D1P1 (#2)

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ mod d1;
 mod d2;
 mod utils;
 
-fn main() { // run using `cargo run d1`
+fn main() -> Result<(), Box<dyn std::error::Error>> { // run using `cargo run d1`
   let args: Vec<String> = std::env::args().collect();
   let day = &args[1];
 
   match day.as_str() {
     "d1" => d1::solve(),
-    _ => println!("Unknown day"),
+    _ => {
+      println!("Unknown day");
+      Ok(())
+    }
   }
 }
