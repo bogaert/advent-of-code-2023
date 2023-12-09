f0ac0854120c208a251dce5e4aefbf2beec61141
Christophe Bogaert <christophe@bogaert.se>
2023-12-09 11:06:28 +0100

    Add solution to D1P1

--- src/d1/mod.rs
+++ src/d1/mod.rs
@@ -1,27 +1,70 @@
 use polars::prelude::*;
 use std::error::Error;
+use std::fs::read_to_string;
+use regex::Regex;
 
-fn print_first_5_lines_with_polars() -> Result<(), Box<dyn Error>> {
-    // Path to the file in the parent directory
-    let path = "input/d1/input";
+pub fn solve() -> Result<(), Box<dyn Error>> {
+    // Read the file
+    let file_content = read_to_string("input/d1/input")?;
 
-    // Load the file into a DataFrame
-    let df = CsvReader::from_path(path)?
-        .infer_schema(None)
-        .has_header(false)
-        .finish()?;
+    // Split the file content into lines
+    let lines: Vec<&str> = file_content.lines().collect();
 
-    // Take the first 5 rows
-    let first_5 = df.head(Some(5));
+    // Create a Series from the lines
+    let series = Series::new("input", lines);
 
-    // Print the DataFrame
-    println!("{:?}", first_5);
+    // Regular expression to find a digit
+    let re_digit = Regex::new(r"\d").unwrap();
+
+    // Find the first digit and the last digit in each string
+    let first_digits: Vec<Option<i32>> = series.utf8()?
+        .into_iter()
+        .map(|line| {
+            line.and_then(|l| re_digit.find(l)
+                .and_then(|m| m.as_str().parse::<i32>().ok()))
+        })
+        .collect();
+
+    let last_digits: Vec<Option<i32>> = series.utf8()?
+        .into_iter()
+        .map(|line| {
+            line.map(|l| l.chars().rev().collect::<String>())
+                .and_then(|rev_l| re_digit.find(&rev_l)
+                .and_then(|m| m.as_str().parse::<i32>().ok()))
+        })
+        .collect();
+
+    // Create Series from the digits
+    let first_digit_series = Series::new("first_digit", &first_digits);
+    let last_digit_series = Series::new("last_digit", &last_digits);
+
+    // Concatenate first_digit and last_digit into a single integer
+    let concatenated_digits: Vec<Option<i32>> = first_digits.into_iter().zip(last_digits.into_iter())
+        .map(|(first, last)| {
+            match (first, last) {
+                (Some(f), Some(l)) => Some(format!("{}{}", f, l).parse::<i32>().unwrap()),
+                _ => None
+            }
+        })
+        .collect();
+
+    let concatenated_series = Series::new("concatenated", concatenated_digits);
+
+    // Create a DataFrame from the Series
+    let mut df = DataFrame::new(vec![series])?;
+    df = df.hstack(&[first_digit_series, last_digit_series, concatenated_series])?;
+
+    // Print the DataFrame to see the output
+    println!("{:?}", df);
+
+    // Sum the values in the 'concatenated' column
+    let sum: i32 = df.column("concatenated")?
+        .i32()?
+        .sum()
+        .unwrap_or(0);
+
+    // Print the sum
+    println!("Sum of concatenated column: {}", sum);
 
     Ok(())
-}
-
-pub fn solve() {
-    if let Err(e) = print_first_5_lines_with_polars() {
-        eprintln!("Error: {}", e);
-    }
 }
