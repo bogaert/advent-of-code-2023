da87ad4bdc6eb5d8b97ccb04c2776d0425d9a80d
Christophe Bogaert <christophe@bogaert.se>
2023-12-09 10:04:37 +0100

    Add structure for Advent of Code

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,13 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+mod d1;
+mod d2;
+mod utils;
 
-fn print_first_5_lines_of_file() -> io::Result<()> {
-    // Path to the file in the parent directory
-    let path = Path::new("input/d1/input");
+fn main() { // run using `cargo run d1`
+  let args: Vec<String> = std::env::args().collect();
+  let day = &args[1];
 
-    // Open the file
-    let file = File::open(&path)?;
-
-    // Use a buffered reader to read the file line-by-line
-    let reader = io::BufReader::new(file);
-
-    // Iterate over the lines and print the first 5
-    for (index, line) in reader.lines().enumerate() {
-        let line = line?;
-        println!("{}", line);
-
-        // Stop after 5 lines
-        if index >= 4 {
-            break;
-        }
-    }
-
-    Ok(())
-}
-
-fn main() {
-    // Call the function in main
-    if let Err(e) = print_first_5_lines_of_file() {
-        eprintln!("Error reading file: {}", e);
-    }
+  match day.as_str() {
+    "d1" => d1::solve(),
+    _ => println!("Unknown day"),
+  }
 }
